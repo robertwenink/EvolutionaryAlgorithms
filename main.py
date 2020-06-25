4ce33720a105a7255c1c2f0d9dcc7ba86b948c31
Berend-Jan Lange <lange.berendjan@gmail.com>
2020-06-25 21:50:28 +0200

    functional plots

--- main.py
+++ main.py
@@ -67,7 +67,8 @@ def get_random_instances(directory):
 
     instances, names = [], []
 
-    for n in [50, 100, 250, 500]:
+    for n in [100]:
+    # for n in [50, 100, 250, 500]:
         for w in [100]:
             for p in [0.5]:
 
@@ -80,35 +81,74 @@ def get_random_instances(directory):
                 names.append(filename)
 
     return names, instances
+
     
 def run_instances(names, instances):
 
     NO_RUNS = 5
-    NO_EVALS = 15000
-    NO_PARTICLES = [25, 50, 100, 250]
+    NO_EVALS = 20000
+
+    tests = [
+        {'c1' : [0.5, 1.0, 2, 5]},
+        {'c2' : [0.5, 1.0, 2, 5]}
+    ]
+
+    test_params = []
+    for k1, v1 in tests[0].items():
+        for val1 in v1:
+            if len(tests) > 1:
+                for k2, v2 in tests[1].items():
+                    for val2 in v2:
+                        if len(tests) > 2:
+                            for k3, v3 in tests[2].items():
+                                for val3 in v3:
+                                    test_params.append({k1: val1, k2: val2, k3: val3})
+                        else:
+                            test_params.append({k1: val1, k2: val2})
+            else:
+                test_params.append({k1: val1})
+
 
     for i, instance in enumerate(instances):
-        for no_particles in NO_PARTICLES:
-            n, w, p = get_params_from_instance_filename(names[i])
 
-            GBO = False
-            local_search = False
-            norm_velo = False
+        n, w, p = get_params_from_instance_filename(names[i])
 
-            hyper_params = {
-                'num_particles' : no_particles,
+        experiment_params = []
+
+        for t in test_params:
+
+            params = {                  # DEFAULT VALUES
+                'num_particles' : 25,
+                'c1': 1.0,
+                'c2': 1.0,
+                'v_bound' : 6.0,
                 'nodes': n,
                 'maxweight': w,
                 'edgeprob': p,
-                'GBO': GBO,
-                'local_search': local_search,
-                'norm_velo': norm_velo
+                'GBO': False,
+                'local_search': False
             }
+
+            for key, value in t.items():
+                params[key] = value
+            experiment_params.append(params)
+
+
+        for hyper_params in experiment_params:
+
+            no_particles = hyper_params['num_particles']
+            c1 = hyper_params['c1']
+            c2 = hyper_params['c2']
+            v_bound = hyper_params['v_bound']
+            GBO = hyper_params['GBO']
+            local_search = hyper_params['local_search']
+
             metrics = Metrics('BPSO', NO_RUNS, NO_EVALS, hyper_params)
+            
             for run in range(NO_RUNS):
                 metrics.run = run
                 VectorizedBinaryParticleSwarmOptimization(instance, metrics, no_particles, 
-                    run).np_run(GBO, local_search, norm_velo)
+                    run, c1, c2, v_bound).np_run(GBO, local_search)
 
             metrics_file_prefix = metrics_filename_generator(hyper_params)
             
